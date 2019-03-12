95aaacac73fbce4b9ee64408f635fab9572996a8
Viachaslau Kniha <viachaslau.kniha@itechart-group.com>
2019-03-12 18:01:15 +0300

    Implemented 1-3 page, problem with img exists

--- giphy.js
+++ giphy.js
@@ -1,20 +1,38 @@
-function Search(searchProperties){
-    const urlEncodedSearchPhrase = encodeURIComponent(searchProperties.searchPhrase);
-    const queryString = `${searchProperties.endPoint}q=${urlEncodedSearchPhrase}&api_key=${searchProperties.apiKey}&limit=${searchProperties.limit}`;
+function memoizeSearch() {
+    const generateKey = (searchPhrase, offset) => `${searchPhrase} : ${offset}`;
+    const cachedResults = {};
 
-    return new Promise(((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-    
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-                resolve(xhr.response);
-            } else {
-                const error = new Error(`Error... responce info:${xhr.status} ${xhr.statusText}`);
-                reject(error);
+    const mSearch = function (searchProperties) {
+        const urlEncodedSearchPhrase = encodeURIComponent(searchProperties.searchPhrase);
+        const queryString = `${searchProperties.endPoint}q=${urlEncodedSearchPhrase}&api_key=${searchProperties.apiKey}&limit=${searchProperties.limit}&offset=${searchProperties.offset}`;
+
+        return new Promise(((resolve, reject) => {
+            const key = generateKey(urlEncodedSearchPhrase, searchProperties.offset);
+            const cachedResult = cachedResults[key];
+
+            if (cachedResult) {
+                console.log(`cached value for key = ${key}`);
+                resolve(cachedResult);
             }
-        };
-    
-        xhr.open('GET', queryString, true);
-        xhr.send();
-    }));
+
+            const xhr = new XMLHttpRequest();
+        
+            xhr.onload = function() {
+                if (xhr.status === 200) {
+                    cachedResults[key] = xhr.response;
+                    resolve(xhr.response);
+                } else {
+                    const error = new Error(`Error... responce info:${xhr.status} ${xhr.statusText}`);
+                    reject(error);
+                }
+            };
+        
+            xhr.open('GET', queryString, true);
+            xhr.send();
+        }));
+    }
+
+    return mSearch;
 }
+
+const search = memoizeSearch();
