7c229995f3699f11f97e0ed2831458664196efb0
Viachaslau Kniha <viachaslau.kniha@itechart-group.com>
2019-03-15 11:09:54 +0300

    test com

--- giphy.js
+++ giphy.js
@@ -75,7 +75,7 @@ function enableSubmitButton(event) {
 function moveToGifPage(gif) {
 
     const gifByIdState = {gifId: gif.id};
-    //history.pushState({gifByIdState: gifByIdState}, "", `/Giphy/gif/${gif.id}`);
+    //history.pushState({gifByIdState: gifByIdState}, "", `gif/${gif.id}`);
     history.pushState({gifByIdState: gifByIdState}, "", null);
 
     document.getElementById("searchContainer").style.display = "none";
@@ -136,7 +136,7 @@ function sendSearchRequest() {
     const offset = 0;
 
     const searchProperties = {searchPhrase, offset, limit};
-    //history.pushState({searchState: searchProperties}, "", `/Giphy/search?q=${searchPhrase}`);
+    //history.pushState({searchState: searchProperties}, "", `search?q=${searchPhrase}`);
     history.pushState({searchState: searchProperties}, "", null);
 
     returnToInitialSearchState(); 
