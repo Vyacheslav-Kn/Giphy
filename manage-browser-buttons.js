a962d5a264bd7fcdf47a963056d1a03b3a9b052d
Viachaslau Kniha <viachaslau.kniha@itechart-group.com>
2019-03-18 14:43:09 +0300

    change file types

--- manage-browser-buttons.js
+++ manage-browser-buttons.js
@@ -1,93 +1,97 @@
 import * as front from './front'
-import { searchGifsByPhrase, searchGifById } from './search'
+import {searchGifById, searchGifsByPhrase} from './search'
 
+// eslint-disable-next-line max-statements
 function preparePageWhenUserComesByReference (currentUrl) {
-  const urlToSearchMethod = 'search?q='
-  const urlToGifMethod = 'gif/'
+    const urlToSearchMethod = 'search?q='
+    const urlToGifMethod = 'gif/'
 
-  // search page reference
-  if (currentUrl.indexOf(urlToSearchMethod) >= 0) {
-    const searchPhrase = currentUrl.substring(currentUrl.indexOf(urlToSearchMethod) + urlToSearchMethod.length)
-    const searchProperties = { searchPhrase, offset: 0, limit: 5 }
-    history.replaceState({ searchState: searchProperties }, '', currentUrl)
+    // Search page reference
+    if (currentUrl.indexOf(urlToSearchMethod) >= 0) {
+        const searchPhrase = currentUrl.substring(currentUrl.indexOf(urlToSearchMethod) + urlToSearchMethod.length)
+        const searchProperties = {searchPhrase, offset: 0, limit: 5}
+        history.replaceState({searchState: searchProperties}, '', currentUrl)
 
-    searchGifsByPhrase(searchProperties).then(response => {
-      front.insertLoadedGifsOnPage(JSON.parse(response).data)
-      front.insertLoadButtonOnPage(searchProperties)
-    }, error => console.log(error))
-    localStorage.removeItem('historyState')
-    localStorage.removeItem('pathName')
-    return
-  }
+        searchGifsByPhrase(searchProperties).then(response => {
+            front.insertLoadedGifsOnPage(JSON.parse(response).data)
+            front.insertLoadButtonOnPage(searchProperties)
+        }, error => console.log(error))
+        localStorage.removeItem('historyState')
+        localStorage.removeItem('pathName')
+        return
+    }
 
-  // gif page reference
-  if (currentUrl.indexOf(urlToGifMethod) >= 0) {
-    const gifId = currentUrl.substring(currentUrl.indexOf(urlToGifMethod) + urlToGifMethod.length)
-    history.replaceState({ isMovedFromMainPage: true, gifState: { gifId } }, '', currentUrl)
+    // Gif page reference
+    if (currentUrl.indexOf(urlToGifMethod) >= 0) {
+        const gifId = currentUrl.substring(currentUrl.indexOf(urlToGifMethod) + urlToGifMethod.length)
+        history.replaceState({isMovedFromMainPage: true, gifState: {gifId}}, '', currentUrl)
 
-    searchGifById(gifId).then(response => {
-      front.insertGifOnPage(JSON.parse(response).data)
-    }, error => console.log(error))
-    localStorage.removeItem('historyState')
-    localStorage.removeItem('pathName')
-    return
-  }
+        searchGifById(gifId).then(response => {
+            front.insertGifOnPage(JSON.parse(response).data)
+        }, error => console.log(error))
+        localStorage.removeItem('historyState')
+        localStorage.removeItem('pathName')
+        return
+    }
 
-  // main page reference
-  front.clearSearchElements()
+    // Main page reference
+    front.clearSearchElements()
 }
 
+// eslint-disable-next-line max-statements
 function preparePageWhenUserComesByPressingBrowserButtons (currentState) {
-  // user comes to search page
-  if (currentState.searchState) {
-    const { searchState } = currentState
-    const numberOfLoadGifsRequests = (searchState.offset / searchState.limit) + 1
+    // User comes to search page
+    if (currentState.searchState) {
+        const {searchState} = currentState
+        const numberOfLoadGifsRequests = (searchState.offset / searchState.limit) + 1
 
-    document.getElementById('results').innerHTML = ''
-    document.getElementById('controlButtons').innerHTML = ''
+        document.getElementById('results').innerHTML = ''
+        document.getElementById('controlButtons').innerHTML = ''
 
-    for (let i = 0; i < numberOfLoadGifsRequests; i++) {
-      searchState.offset = searchState.limit * i
+        for (let i = 0; i < numberOfLoadGifsRequests; i++) {
+            searchState.offset = searchState.limit * i
 
-      searchGifsByPhrase(searchState).then(response => {
-        front.insertLoadedGifsOnPage(JSON.parse(response).data)
-      }, error => console.log(error))
+            searchGifsByPhrase(searchState).then(response => {
+                front.insertLoadedGifsOnPage(JSON.parse(response).data)
+            }, error => console.log(error))
+        }
+
+        front.insertLoadButtonOnPage(searchState)
+
+        document.getElementById('singleGif').style.display = 'none'
+        document.getElementById('authorInfo').style.display = 'none'
+        document.getElementById('searchContainer').style.display = ''
     }
 
-    front.insertLoadButtonOnPage(searchState)
-
-    document.getElementById('singleGif').style.display = 'none'
-    document.getElementById('authorInfo').style.display = 'none'
-    document.getElementById('searchContainer').style.display = ''
-  }
-
-  // user comes to gif page
-  if (currentState.gifState) {
-    const gifState = currentState.gifState
-    searchGifById(gifState.gifId).then(response => {
-      front.insertGifOnPage(JSON.parse(response).data)
-    }, error => console.log(error))
-  }
+    // User comes to gif page
+    if (currentState.gifState) {
+        const {gifState} = currentState
+        searchGifById(gifState.gifId).then(response => {
+            front.insertGifOnPage(JSON.parse(response).data)
+        }, error => console.log(error))
+    }
 }
 
+// eslint-disable-next-line max-statements
 export function manageBrowserButtons () {
-  let currentState = history.state
-  const currentUrl = localStorage.getItem('pathName') === null
-    ? window.location.href
-    : `${JSON.parse(localStorage.getItem('pathName')).replace('/Giphy/', '')}`
+    let currentState = history.state
+    let currentUrl = window.location.href 
+    if (localStorage.getItem('pathName') !== null) {
+        currentUrl = `${JSON.parse(localStorage.getItem('pathName')).replace('/Giphy/', '')}`;
+    } 
 
-  if (history.state === null) {
-    if (JSON.parse(localStorage.getItem('historyState')) === null) {
-      preparePageWhenUserComesByReference(currentUrl)
-      return
+    if (history.state === null) {
+        if (JSON.parse(localStorage.getItem('historyState')) === null) {
+            preparePageWhenUserComesByReference(currentUrl)
+            return
+        }
+
+        // User comes after refreshing some page -> take state from local storage
+        currentState = JSON.parse(localStorage.getItem('historyState'))
+        history.replaceState(currentState, '', currentUrl)
+        localStorage.removeItem('historyState')
+        localStorage.removeItem('pathName')
     }
 
-    // user comes after refreshing some page -> take state from local storage
-    currentState = JSON.parse(localStorage.getItem('historyState'))
-    history.replaceState(currentState, '', currentUrl)
-    localStorage.removeItem('historyState')
-    localStorage.removeItem('pathName')
-  }
-
-  preparePageWhenUserComesByPressingBrowserButtons(currentState)
+    preparePageWhenUserComesByPressingBrowserButtons(currentState)
 }
--- search.js
+++ search.js
@@ -1,63 +1,63 @@
 function memoizeSearch () {
-  const generateKey = (searchPhrase, offset) => `${searchPhrase} : ${offset}`
-  const cachedResults = {}
+    const generateKey = (searchPhrase, offset) => `${searchPhrase} : ${offset}`
+    const cachedResults = {}
 
-  const mSearch = function ({ searchPhrase, limit, offset }) {
-    const apiKey = 'hBZX9D1GD3KQfZ5jwDWAyEYsqnQIIEIJ'
-    const endPoint = 'https://api.giphy.com/v1/gifs/search?'
+    const mSearch = function ({searchPhrase, limit, offset}) {
+        const apiKey = 'hBZX9D1GD3KQfZ5jwDWAyEYsqnQIIEIJ'
+        const endPoint = 'https://api.giphy.com/v1/gifs/search?'
 
-    const queryString = `${endPoint}q=${searchPhrase}&api_key=${apiKey}&limit=${limit}&offset=${offset}`
+        const queryString = `${endPoint}q=${searchPhrase}&api_key=${apiKey}&limit=${limit}&offset=${offset}`
 
-    return new Promise((resolve, reject) => {
-      const key = generateKey(searchPhrase, offset)
-      const cachedResult = cachedResults[key]
+        return new Promise((resolve, reject) => {
+            const key = generateKey(searchPhrase, offset)
+            const cachedResult = cachedResults[key]
 
-      if (cachedResult) {
-        console.log(`cached value for key = ${key}`)
-        resolve(cachedResult)
-      }
+            if (cachedResult) {
+                console.log(`cached value for key = ${key}`)
+                resolve(cachedResult)
+            }
 
-      const xhr = new XMLHttpRequest()
+            const xhr = new XMLHttpRequest()
 
-      xhr.onload = function () {
-        if (xhr.status === 200) {
-          cachedResults[key] = xhr.response
-          resolve(xhr.response)
-        } else {
-          const error = new Error(`Error... response info:${xhr.status} ${xhr.statusText}`)
-          reject(error)
-        }
-      }
+            xhr.onload = function () {
+                if (xhr.status === 200) {
+                    cachedResults[key] = xhr.response
+                    resolve(xhr.response)
+                } else {
+                    const error = new Error(`Error... response info:${xhr.status} ${xhr.statusText}`)
+                    reject(error)
+                }
+            }
 
-      xhr.open('GET', queryString, true)
-      xhr.send()
-    })
-  }
+            xhr.open('GET', queryString, true)
+            xhr.send()
+        })
+    }
 
-  return mSearch
+    return mSearch
 }
 
 export const searchGifsByPhrase = memoizeSearch()
 
 export function searchGifById (id) {
-  return new Promise((resolve, reject) => {
-    const apiKey = 'hBZX9D1GD3KQfZ5jwDWAyEYsqnQIIEIJ'
-    const endPoint = 'https://api.giphy.com/v1/gifs/'
+    return new Promise((resolve, reject) => {
+        const apiKey = 'hBZX9D1GD3KQfZ5jwDWAyEYsqnQIIEIJ'
+        const endPoint = 'https://api.giphy.com/v1/gifs/'
 
-    const queryString = `${endPoint}${id}?api_key=${apiKey}`
+        const queryString = `${endPoint}${id}?api_key=${apiKey}`
 
-    const xhr = new XMLHttpRequest()
+        const xhr = new XMLHttpRequest()
 
-    xhr.onload = function () {
-      if (xhr.status === 200) {
-        resolve(xhr.response)
-      } else {
-        const error = new Error(`Error... responce info:${xhr.status} ${xhr.statusText}`)
-        reject(error)
-      }
-    }
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                resolve(xhr.response)
+            } else {
+                const error = new Error(`Error... responce info:${xhr.status} ${xhr.statusText}`)
+                reject(error)
+            }
+        }
 
-    xhr.open('GET', queryString, true)
-    xhr.send()
-  })
+        xhr.open('GET', queryString, true)
+        xhr.send()
+    })
 }
