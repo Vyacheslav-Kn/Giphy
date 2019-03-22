08b8000f634c15718f64e9d1c27d0af1ab50cd9a
Viachaslau Kniha <viachaslau.kniha@itechart-group.com>
2019-03-22 12:07:54 +0300

    fix clonning of state with spread

--- manipulate-pages.js
+++ manipulate-pages.js
@@ -48,7 +48,7 @@ export function preparePagesForNonUserInitiatedTransition (currentUrl) {
 
 function openOldSearchPage (currentState) {
     const {searchState} = currentState
-    const modifiedSearchState = Object.assign(currentState.searchState)
+    const modifiedSearchState = {...currentState.searchState}
 
     const numberOfLoadGifsRequests = (searchState.offset / searchState.limit) + 1
     modifiedSearchState.limit = numberOfLoadGifsRequests * searchState.limit
