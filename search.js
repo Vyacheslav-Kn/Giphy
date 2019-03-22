f0283218238a039ca1f87c06da87225b1e05e4bb
Viachaslau Kniha <viachaslau.kniha@itechart-group.com>
2019-03-22 11:52:43 +0300

    fixed key generation

--- search.js
+++ search.js
@@ -4,32 +4,21 @@ export const defaultLimit = 5
 export const defaultOffset = 0
 
 function memoizeSearch () {
-    const generateKey = (searchPhrase, offset) => `${searchPhrase} : ${offset}`
+    const generateKey = (searchPhrase, offset, limit) => `${searchPhrase} : ${offset} : ${limit}`
     const cachedResults = {}
 
     const mSearch = function ({searchPhrase, limit, offset}) {
         const queryString = `${endPoint}${urlToSearchMethod}${searchPhrase}&api_key=${apiKey}&limit=${limit}&offset=${offset}`
 
-        /*
-        const key = generateKey(searchPhrase, offset)
+        const key = generateKey(searchPhrase, offset, limit)
         const cachedResult = cachedResults[key]
 
         if (cachedResult) {
             console.log(`cached value for key = ${key}`)
-            return Promise.resolve(cachedResult)
+            Promise.resolve(cachedResult)
         }
-        */
 
         return new Promise((resolve) => {
-            const key = generateKey(searchPhrase, offset)
-            const cachedResult = cachedResults[key]
-
-            if (cachedResult) {
-                console.log(`cached value for key = ${key}`)
-                resolve(cachedResult)
-            }
-
-
             fetch(queryString)
                 .then(response => response.json())
                 .then(responseJSON => {
